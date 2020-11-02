b32027a72c8b2b7a65893aba421c5e0a6a61db45
Hugo Morelle <hugo.morelle@gmail.com>
2020-11-02 14:20:02 +0100

    Create tester.js

---
+++ tester.js
@@ -0,0 +1,38 @@
+
+/*
+* tester.js
+* V1.0
+* 02/11/2020
+* Hugo MORELLE
+Functions :
+addCSS()
+removeElement()
+
+*/
+
+/*
+ * Insert/replace CSS in the DOM
+ * @param  {String}   content The css content
+ * @param  {String}   id      The id of the injected style
+ */
+var addCSS = function (content, id){
+  var styleSheet = document.createElement("style");
+  if(id){
+    styleSheet.id = id;
+    if (document.querySelector(`style#${id}`) !== null) { 
+      removeElement(`style#${id}`) 
+    }
+  }
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+}
+
+/*
+ * Remove element from the DOM
+ * @param  {String} selector The element selector
+ * @param  {Node}   parent   The parent to search in [optional]
+ * @return {Node}            The element
+ */
+removeElement = function (selector) {
+  document.querySelector(selector).parentElement.removeChild(document.querySelector(selector))
+}
