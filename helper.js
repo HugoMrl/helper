0b7e9286154292832048d0f96a8287feb9142755
Hugo Morelle <hugo.morelle@gmail.com>
2020-12-08 19:42:36 +0100

    added waitUntil

--- helper.js
+++ helper.js
@@ -1,30 +1,34 @@
 /*
-* Helpers.JS
-* V1.1
-* 02/11/2020
-* Hugo MORELLE
+ * Helpers.JS
+ * V1.1
+ * 08/12/2020
+ * Hugo MORELLE
+ *
+ * Functions :
+ */
 
-Functions :
-$()
-$$()
-dedupe()
-eventGA()
-getCookie()
-getOffsetTop()
-getParams()
-httpGet()
-isInViewport()
-isOutOfViewport()
-onClickOrTap()
-randomNumber()
-ready()
-round()
-scrollDistance()
-scrollStop()
-setCookie()
-shuffle()
-slugify()
-*/
+export {
+  $,
+  $$,
+  dedupe,
+  eventGA,
+  getCookie,
+  getOffsetTop,
+  getParams,
+  httpGet,
+  isInViewport,
+  isOutOfViewport,
+  onClickOrTap,
+  randomNumber,
+  ready,
+  round,
+  scrollDistance,
+  scrollStop,
+  setCookie,
+  shuffle,
+  slugify,
+  waitUntil
+};
 
 /*
  * Get the first matching element in the DOM
@@ -32,7 +36,7 @@ slugify()
  * @param  {Node}   parent   The parent to search in [optional]
  * @return {Node}            The element
  */
-var $ = function (selector, parent) {
+function $(selector, parent) {
   return (parent ? parent : document).querySelector(selector);
 };
 
@@ -42,7 +46,7 @@ var $ = function (selector, parent) {
  * @param  {Node}   parent   The parent to search in [optional]
  * @return {Array}           Th elements
  */
-var $$ = function (selector, parent) {
+function $$(selector, parent) {
   return Array.prototype.slice.call((parent ? parent : document).querySelectorAll(selector));
 };
 
@@ -52,23 +56,19 @@ var $$ = function (selector, parent) {
  * @param  {Array} array The array to shuffle
  * @return {String}      The first item in the shuffled array
  */
-var shuffle = function (array) {
-
+function shuffle(array) {
   var currentIndex = array.length;
   var temporaryValue, randomIndex;
-
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 };
 
@@ -77,7 +77,7 @@ var shuffle = function (array) {
  * @param  {Array} arr The array
  * @return {Array}     A new array with duplicates removed
  */
-var dedupe = function (arr) {
+function dedupe(arr) {
   return arr.filter(function (item, index) {
     return arr.indexOf(item) === index;
   });
@@ -91,10 +91,12 @@ var dedupe = function (arr) {
  * @param {String}  trackerName  The tracker name
  * @param {Boolean}  interactive  The event interactivity, causing bounce or not
  */
-var eventGA = function (category, action, label, trackerName = "", interactive = true) {
+function eventGA(category, action, label, trackerName = "", interactive = true) {
   var tracker = trackerName ? trackerName + ".send" : "send";
   var interactivity = interactive ? 1 : 0;
-  window[window['GoogleAnalyticsObject']](tracker, 'event', category, action, label, { 'nonInteraction': interactivity });
+  window[window['GoogleAnalyticsObject']](tracker, 'event', category, action, label, {
+    'nonInteraction': interactivity
+  });
 }
 
 /*
@@ -102,7 +104,7 @@ var eventGA = function (category, action, label, trackerName = "", interactive =
  * @param  {String} name  The name of the cookie
  * @return {String}       The cookie value
  */
-var getCookie = function (name) {
+function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
@@ -114,7 +116,7 @@ var getCookie = function (name) {
  * @param  {Node} elem The element
  * @return {Number}    Distance from the top in pixels
  */
-var getOffsetTop = function (elem) {
+function getOffsetTop(elem) {
   var location = 0;
   if (elem.offsetParent) {
     while (elem) {
@@ -130,7 +132,7 @@ var getOffsetTop = function (elem) {
  * @param  {String} url The URL
  * @return {Object}     The URL parameters
  */
-var getParams = function (url) {
+function getParams(url) {
   var params = {};
   var parser = document.createElement('a');
   parser.href = url ? url : window.location.href;
@@ -153,7 +155,7 @@ var getParams = function (url) {
  * @param  {String}  user       The request user (optional)
  * @param  {String}  password   The request password (optional)
  */
-var httpGet = function (base, params, body=null, user = null, password = null) {
+function httpGet(base, params, body = null, user = null, password = null) {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function () {
     // Si la requête est complète et le retour est OK
@@ -163,8 +165,10 @@ var httpGet = function (base, params, body=null, user = null, password = null) {
       //Do something
     }
   }
-  request.open("GET", base+"?"+params, true, user, password); // true pour une requête asynchrone
-  if(user!=null){request.withCredentials = true};
+  request.open("GET", base + "?" + params, true, user, password); // true pour une requête asynchrone
+  if (user != null) {
+    request.withCredentials = true
+  };
   request.send(body);
 };
 
@@ -174,7 +178,7 @@ var httpGet = function (base, params, body=null, user = null, password = null) {
  * @param  {Node}    elem The element
  * @return {Boolean}      Returns true if element is in the viewport
  */
-var isInViewport = function (elem) {
+function isInViewport(elem) {
   var distance = elem.getBoundingClientRect();
   return (
     distance.top >= 0 &&
@@ -190,11 +194,9 @@ var isInViewport = function (elem) {
  * @param  {Node}  elem The element to check
  * @return {Object}     A set of booleans for each side of the element
  */
-var isOutOfViewport = function (elem) {
-
+function isOutOfViewport(elem) {
   // Get element's bounding
   var bounding = elem.getBoundingClientRect();
-
   // Check if it's out of the viewport on each side
   var out = {};
   out.top = bounding.top < 0;
@@ -203,9 +205,7 @@ var isOutOfViewport = function (elem) {
   out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
   out.any = out.top || out.left || out.bottom || out.right;
   out.all = out.top && out.left && out.bottom && out.right;
-
   return out;
-
 };
 
 
@@ -214,14 +214,11 @@ var isOutOfViewport = function (elem) {
  * @param  {Node}   elem       The element to listen for clicks and taps on
  * @param  {Function} callback The callback function to run on a click or tap
  */
-var onClickOrTap = function (elem, callback) {
-
+function onClickOrTap(elem, callback) {
   // Make sure a callback is provided
   if (!callback || typeof (callback) !== 'function') return;
-
   // Variables
   var isTouch, startX, startY, distX, distY;
-
   /**
    * touchstart handler
    * @param  {event} event The touchstart event
@@ -229,28 +226,22 @@ var onClickOrTap = function (elem, callback) {
   var onTouchStartEvent = function (event) {
     // Disable click event
     isTouch = true;
-
     // Get the starting location and time when finger first touches surface
     startX = event.changedTouches[0].pageX;
     startY = event.changedTouches[0].pageY;
   };
-
   /**
    * touchend handler
    * @param  {event} event The touchend event
    */
   var onTouchEndEvent = function (event) {
-
     // Get the distance travelled and how long it took
     distX = event.changedTouches[0].pageX - startX;
     distY = event.changedTouches[0].pageY - startY;
-
     // If a swipe happened, do nothing
     if (Math.abs(distX) >= 7 || Math.abs(distY) >= 10) return;
-
     // Run callback
     callback(event);
-
   };
 
   /**
@@ -263,16 +254,13 @@ var onClickOrTap = function (elem, callback) {
       isTouch = false;
       return;
     }
-
     // Run our callback
     callback(event);
   };
-
   // Event listeners
   elem.addEventListener('touchstart', onTouchStartEvent, false);
   elem.addEventListener('touchend', onTouchEndEvent, false);
   elem.addEventListener('click', onClickEvent, false);
-
 };
 
 /*
@@ -281,7 +269,7 @@ var onClickOrTap = function (elem, callback) {
  * @param  {Integer} max  The maximum value
  * @return {Integer}      A random number
  */
-var randomNumber = function (min, max) {
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
@@ -289,19 +277,15 @@ var randomNumber = function (min, max) {
  * Run event after the DOM is ready
  * @param  {Function} fn Callback function
  */
-var ready = function (fn) {
-
+function ready(fn) {
   // Sanity check
   if (typeof fn !== 'function') return;
-
   // If document is already loaded, run method
   if (document.readyState === 'interactive' || document.readyState === 'complete') {
     return fn();
   }
-
   // Otherwise, wait until document is loaded
   document.addEventListener('DOMContentLoaded', fn, false);
-
 };
 
 /*
@@ -311,30 +295,24 @@ var ready = function (fn) {
  * @param  {String}        method    The rounding method (up, down, or auto - defaults to auto) [optional]
  * @return {String}                  The rounded, delimited number
  */
-var round = function (num, precision, method) {
-
+function round(num, precision, method) {
   // Convert string numbers to a float
   num = parseFloat(num);
-
   // If there's no rounding precision, return the number
   if (!precision) return num.toLocaleString();
-
   // Possible methods and their values
   var methods = {
     auto: 'round',
     up: 'ceil',
     down: 'floor'
   };
-
   // Get the method function
   var fn = methods[method];
   if (!fn) {
     fn = 'round';
   }
-
   // Do math!
   return (Math[fn](num / precision) * precision).toLocaleString();
-
 };
 
 /*
@@ -342,74 +320,53 @@ var round = function (num, precision, method) {
  * @param  {Function} callback The callback function to run
  * @param  {Integer}  refresh  How long to wait between scroll events [optional]
  */
-var scrollDistance = function (callback, refresh) {
-
+function scrollDistance(callback, refresh) {
   // Make sure a valid callback was provided
   if (!callback || typeof callback !== 'function') return;
-
   // Variables
   var isScrolling, start, end, distance;
-
   // Listen for scroll events
   window.addEventListener('scroll', function (event) {
-
     // Set starting position
     if (!start) {
       start = window.pageYOffset;
     }
-
     // Clear our timeout throughout the scroll
     window.clearTimeout(isScrolling);
-
     // Set a timeout to run after scrolling ends
     isScrolling = setTimeout(function () {
-
       // Calculate distance
       end = window.pageYOffset;
       distance = end - start;
-
       // Run the callback
       callback(distance, start, end);
-
       // Reset calculations
       start = null;
       end = null;
       distance = null;
-
     }, refresh || 66);
-
   }, false);
-
 };
 
 /*
  * Run a callback function after scrolling has stopped
  * @param  {Function} callback The function to run after scrolling
  */
-var scrollStop = function (callback) {
-
+function scrollStop(callback) {
   // Make sure a valid callback was provided
   if (!callback || typeof callback !== 'function') return;
-
   // Setup scrolling variable
   var isScrolling;
-
   // Listen for scroll events
   window.addEventListener('scroll', function (event) {
-
     // Clear our timeout throughout the scroll
     window.clearTimeout(isScrolling);
-
     // Set a timeout to run after scrolling ends
     isScrolling = setTimeout(function () {
-
       // Run the callback
       callback();
-
     }, 66);
-
   }, false);
-
 };
 
 /*
@@ -417,7 +374,7 @@ var scrollStop = function (callback) {
  * @param  {String} name  The name of the cookie
  * @param  {String} value  The cookie value
  */
-var setCookie = function (name, value, expirationInSeconds, path = "/") {
+function setCookie(name, value, expirationInSeconds, path = "/") {
   var date = new Date();
   date.setTime(date.getTime() + (expirationInSeconds * 1000));
   date.toGMTString()
@@ -425,12 +382,11 @@ var setCookie = function (name, value, expirationInSeconds, path = "/") {
 };
 
 /*
- *
  * Slugify and remove all diacritics from a string
  * @param {String} string to replace
  * @return {String} string replaced
  */
-var slugify = function (str) {
+function slugify(str) {
   var map = {
     '-': ' ',
     '-': '_',
@@ -447,3 +403,27 @@ var slugify = function (str) {
   };
   return str;
 };
+
+/*
+ * Wait for predicate to return true, then execute success or error
+ * @param {Function} predicate function
+ * @param {Function} success function
+ * @param {Function} error function
+ */
+function waitUntil(predicate, success, error) {
+  var interval = setInterval(function () {
+    if (predicate()) {
+      clearInterval(interval);
+      interval = null;
+      success();
+    }
+  }, 33);
+  setTimeout(function () {
+    if (interval !== null) {
+      clearInterval(interval);
+      if (typeof (error) === 'function') {
+        error();
+      }
+    }
+  }, 10000);
+}
